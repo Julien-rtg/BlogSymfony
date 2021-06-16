8b256b9bbafe3fec78f7e4e8d178e89cbe3640af
Julien-rtg <julien.rittl@gmail.com>
2021-06-16 18:19:27 +0200

    add some product and display on home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,9 +15,14 @@ class HomeController extends AbstractController {
     /**
      * @Route("/", name="homepage")
     */
-    public function home(){
+    public function home(ProductRepository $productRepository){
+        $productRepository = $this->getDoctrine()->getRepository(Product::class);
+        $products = $productRepository->findAll();
         return $this->render(
-            'users/home.html.twig'
+            'users/home.html.twig',
+            [
+                'products' => $products
+            ]
         );
     }
 
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,19 +2,36 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProductController extends AbstractController
 {
+    
     /**
-     * @Route("/product", name="product")
+     * @Route("/add-product", name="create_product")
      */
-    public function index(): Response
+    public function createProduct(EntityManagerInterface $entityManager): Response
     {
-        return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
-        ]);
+        // you can fetch the EntityManager via $this->getDoctrine()
+        // or you can add an argument to the action: createProduct(EntityManagerInterface $entityManager)
+
+        $product = new Product();
+        $product->setName('Casse croute');
+        $product->setPrice(2130);
+        $product->setDescription('Oui Oui très bon!');
+        $product->setCategory('Snack');
+        $product->setImage('https://via.placeholder.com/350x350');
+        
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($product);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return new Response('Saved new product with id '. $product->getId() . ' and name ' . $product->getName());
     }
 }

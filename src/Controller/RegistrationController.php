d0c3f1666ce0b2d8ade70b262906c17ce02637a4
Julien-rtg <julien.rittl@gmail.com>
2021-07-05 14:43:44 +0200

    forgot password feature

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -10,7 +10,6 @@ use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;

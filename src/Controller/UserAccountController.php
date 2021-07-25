07392c13f86b6d661dc42f7cb2b05f0bb360d558
Julien-Rtg <julien.rittl@gmail.com>
2021-07-25 14:03:32 +0200

    edit contact details on user account

--- src/Controller/UserAccountController.php
+++ src/Controller/UserAccountController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\EditAccountType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,12 +19,54 @@ class UserAccountController extends AbstractController
         if(!$this->getUser()){
             return $this->redirectToRoute('app_login');
         }
+    
+        return $this->render('security/userAccount.html.twig');
 
+    }
+
+    /**
+     * @Route("/user/account/edit", name="user_edit")
+     */
+    public function editAccount(Request $request): Response{
         
-        {
-            return $this->render('users/userAccount.html.twig');
+        $form = $this->createForm(EditAccountType::class, $this->getUser());
+        $email = $this->getUser()->getEmail();
+        $isverif = $this->getUser()->getIsverified();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $user = $form->getData();
+            $emailModify = $form->get('email')->getData();
+            $isverifMod = $form->get('is_verified')->getData();
+            
+            if($isverif != $isverifMod){ // GENERATING ERROR IF USER WANT TO HACK VERIF BY DEVCONSOLE
+                $this->addFlash(
+                    'danger',
+                    "Do not try this",
+                );
+                return $this->redirectToRoute('user_account');
+            }
+            if($emailModify != $email) {
+                $user->setIsverified(false);
+            }
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash(
+                'success',
+                "Contact Details have been modified successfully !",
+            );
+
+            return $this->redirectToRoute('user_account');
         }
 
+        return $this->render('security/userEdit.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 
 }
---
+++ src/Form/EditAccountType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EditAccountType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class)
+            ->add('firstName', TextType::class)
+            ->add('lastName', TextType::class)
+            ->add('is_verified', HiddenType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
